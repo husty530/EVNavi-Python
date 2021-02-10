342c4eba907fb9bed4133049659adb17b35a4fac
husty530 <husty530@gmail.com>
2021-02-10 15:12:27 +0900

    *

--- media.py
+++ media.py
@@ -2,27 +2,13 @@ import cv2
 
 class Capture:
 
-    isOpened = False
-    width = 0
-    height = 0
-    fps = 0
-    frameCount = 0
-
-    def __init__(self, src):
-        self.cap = cv2.VideoCapture(src)
-        Capture.isOpened = self.cap.isOpened()
-        Capture.width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        Capture.height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        Capture.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        Capture.frameCount = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    
     def __init__(self, src, size):
         self.cap = cv2.VideoCapture(src)
-        Capture.isOpened = self.cap.isOpened()
-        Capture.width = size[0]
-        Capture.height = size[1]
-        Capture.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        Capture.frameCount = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        self.isOpened = self.cap.isOpened()
+        self.width = size[0]
+        self.height = size[1]
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.frameCount = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
     
     def read(self):
         _, frame = self.cap.read()
